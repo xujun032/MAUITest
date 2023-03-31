25d3b2b730cfdd42c766cbecb3c77dfc7e93723d
Administrator <Administrator@MS-RSKFBNYUKXPY>
2023-03-31 09:43:26 +0800

    添加项目文件。

---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui_Masa;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Maui_Masa;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
